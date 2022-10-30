9e7eaa4fc7b4cb2b5ddac605e8e17bfe08c9f793
Alena <alena.rackitina2010@yandex.ru>
2022-10-30 15:18:19 +0300

    программа

---
+++ prog1/Program.cs
@@ -0,0 +1,13 @@
+﻿string [] array = {"hello", "2", "world", ":-)"};
+var resultArray = new string[array.Length];
+int index = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        resultArray[index] = value;
+        index++;
+    }
+}
+ 
+Console.WriteLine(string.Join(Environment.NewLine, resultArray, 0, index));
